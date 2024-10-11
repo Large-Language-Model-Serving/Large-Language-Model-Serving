29a2d59939cce5e52dee7341a7ab57dc05c80e6a
Qi Zhang <zh565749@dal.ca>
2024-10-11 16:27:54 -0400

    Create rust project

---
+++ target/debug/back-end-inference-service.d
@@ -0,0 +1,1 @@
+/Users/zhangqi/Desktop/ECE1724_Rust/Group\ Project/back-end-inference-service/target/debug/back-end-inference-service: /Users/zhangqi/Desktop/ECE1724_Rust/Group\ Project/back-end-inference-service/src/main.rs
