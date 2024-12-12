11132e701fd8202f39b06a4e557855c06fce9a52
Qi <feihuvxv@gmail.com>
2024-12-12 18:36:02 -0500

    Merged gemma and qwen into one file

--- src/bin/model.rs
+++ src/bin/model.rs
@@ -7,8 +7,12 @@ extern crate accelerate_src;
 use anyhow::{Error as E, Result};
 use clap::Parser;
 
-use candle_transformers::models::gemma::{Config as Config1, Model as Model1};
-use candle_transformers::models::gemma2::{Config as Config2, Model as Model2};
+use candle_transformers::models::gemma::{Config as ConfigGemma, Model as ModelGemma};
+use candle_transformers::models::gemma2::{Config as ConfigGemma2, Model as ModelGemma2};
+
+use candle_transformers::models::qwen2::{Config as ConfigQwen, ModelForCausalLM as ModelQwen};
+use candle_transformers::models::qwen2_moe::{Config as ConfigQwenMoe, Model as ModelQwenMoe};
+
 
 use candle_core::{DType, Device, Tensor};
 use candle_examples::token_output_stream::TokenOutputStream;
@@ -19,64 +23,139 @@ use tokenizers::Tokenizer;
 
 #[derive(Clone, Debug, Copy, PartialEq, Eq, clap::ValueEnum)]
 enum Which {
-    #[value(name = "2b")]
-    Base2B,
-    #[value(name = "7b")]
-    Base7B,
-    #[value(name = "2b-it")]
-    Instruct2B,
-    #[value(name = "7b-it")]
-    Instruct7B,
-    #[value(name = "1.1-2b-it")]
-    InstructV1_1_2B,
-    #[value(name = "1.1-7b-it")]
-    InstructV1_1_7B,
-    #[value(name = "code-2b")]
-    CodeBase2B,
-    #[value(name = "code-7b")]
-    CodeBase7B,
-    #[value(name = "code-2b-it")]
-    CodeInstruct2B,
-    #[value(name = "code-7b-it")]
-    CodeInstruct7B,
-    #[value(name = "2-2b")]
-    BaseV2_2B,
-    #[value(name = "2-2b-it")]
-    InstructV2_2B,
-    #[value(name = "2-9b")]
-    BaseV2_9B,
-    #[value(name = "2-9b-it")]
-    InstructV2_9B,
+    // Gemma
+    #[value(name = "gemma-2b")]
+    Gemma2B,
+    #[value(name = "gemma-7b")]
+    Gemma7B,
+    #[value(name = "gemma-2b-it")]
+    GemmaInstruct2B,
+    #[value(name = "gemma-7b-it")]
+    GemmaInstruct7B,
+    #[value(name = "gemma-1.1-2b-it")]
+    GemmaInstructV1_1_2B,
+    #[value(name = "gemma-1.1-7b-it")]
+    GemmaInstructV1_1_7B,
+    #[value(name = "gemma-code-2b")]
+    GemmaCode2B,
+    #[value(name = "gemma-code-7b")]
+    GemmaCode7B,
+    #[value(name = "gemma-code-2b-it")]
+    GemmaCodeInstruct2B,
+    #[value(name = "gemma-code-7b-it")]
+    GemmaCodeInstruct7B,
+    #[value(name = "gemma2-2b")]
+    Gemma2_2B,
+    #[value(name = "gemma2-2b-it")]
+    Gemma2Instruct2B,
+    #[value(name = "gemma2-9b")]
+    Gemma2_9B,
+    #[value(name = "gemma2-9b-it")]
+    Gemma2Instruct9B,
+
+    // Qwen
+    #[value(name = "Qwen0.5b")]
+    Qwen0_5b,
+    #[value(name = "Qwen1.8b")]
+    Qwen1_8b,
+    #[value(name = "Qwen4b")]
+    Qwen4b,
+    #[value(name = "Qwen7b")]
+    Qwen7b,
+    #[value(name = "Qwen14b")]
+    Qwen14b,
+    #[value(name = "Qwen72b")]
+    Qwen72b,
+    #[value(name = "Qwen-moe-a2.7b")]
+    QwenMoeA27b,
+    #[value(name = "Qwen2-0.5b")]
+    Qwen2_0_5b,
+    #[value(name = "Qwen2-1.5b")]
+    Qwen2_1_5b,
+    #[value(name = "Qwen2-7b")]
+    Qwen2_7b,
+    #[value(name = "Qwen2-72b")]
+    Qwen2_72b,
 }
 
 impl Which {
-    fn is_v1(&self) -> bool {
+    fn is_gemma(&self) -> bool {
         match self {
-            Self::Base2B
-            | Self::Base7B
-            | Self::Instruct2B
-            | Self::Instruct7B
-            | Self::InstructV1_1_2B
-            | Self::InstructV1_1_7B
-            | Self::CodeBase2B
-            | Self::CodeBase7B
-            | Self::CodeInstruct2B
-            | Self::CodeInstruct7B => true,
-            Self::BaseV2_2B | Self::InstructV2_2B | Self::BaseV2_9B | Self::InstructV2_9B => false,
+            Self::Gemma2B
+            | Self::Gemma7B
+            | Self::GemmaInstruct2B
+            | Self::GemmaInstruct7B
+            | Self::GemmaInstructV1_1_2B
+            | Self::GemmaInstructV1_1_7B
+            | Self::GemmaCode2B
+            | Self::GemmaCode7B
+            | Self::GemmaCodeInstruct2B
+            | Self::GemmaCodeInstruct7B => true,
+            _ => false,
+        }
+    }
+    fn is_gemma2(&self) -> bool {
+        match self {
+            Self::Gemma2_2B | Self::Gemma2Instruct2B | Self::Gemma2_9B | Self::Gemma2Instruct9B => true,
+            _ => false,
+        }
+    }
+
+    fn is_qwen(&self) -> bool {
+        match self {
+            Self::Qwen0_5b
+            | Self::Qwen1_8b
+            | Self::Qwen4b
+            | Self::Qwen7b
+            | Self::Qwen14b
+            | Self::Qwen72b
+            | Self::QwenMoeA27b
+            | Self::Qwen2_0_5b
+            | Self::Qwen2_1_5b
+            | Self::Qwen2_7b
+            | Self::Qwen2_72b => true,
+            _ => false,
+        }
+    }
+
+    fn is_qwen_moe(&self) -> bool {
+        match self {
+            Self::QwenMoeA27b => true,
+            _ => false,
         }
     }
 }
 
 enum Model {
-    V1(Model1),
-    V2(Model2),
+    Gemma(ModelGemma),
+    Gemma2(ModelGemma2),
+    Qwen(ModelQwen),
+    QwenMoe(ModelQwenMoe),
 }
 
 impl Model {
     fn forward(&mut self, input_ids: &Tensor, pos: usize) -> candle_core::Result<Tensor> {
         match self {
-            Self::V1(m) => m.forward(input_ids, pos),
-            Self::V2(m) => m.forward(input_ids, pos),
+            Self::Gemma(m) => m.forward(input_ids, pos),
+            Self::Gemma2(m) => m.forward(input_ids, pos),
+            Self::QwenMoe(ref mut m) => m.forward(input_ids, pos),
+            Self::Qwen(ref mut m) => m.forward(input_ids, pos),
+        }
+    }
+
+    fn is_gemma(&self) -> bool {
+        match self {
+            Self::Gemma(_) => true,
+            Self::Gemma2(_) => true,
+            _ => false,
+        }
+    }
+
+    fn is_qwen(&self) -> bool {
+        match self {
+            Self::Qwen(_) => true,
+            Self::QwenMoe(_) => true,
+            _ => false,
         }
     }
 }
@@ -131,10 +210,20 @@ impl TextGeneration {
         std::io::stdout().flush()?;
 
         let mut generated_tokens = 0usize;
-        let eos_token = match self.tokenizer.get_token("<eos>") {
-            Some(token) => token,
-            None => anyhow::bail!("cannot find the <eos> token"),
+        let eos_token: u32 = if self.model.is_qwen() {
+            match self.tokenizer.get_token("<|endoftext|>") {
+                Some(token) => token,
+                None => anyhow::bail!("cannot find the <|endoftext|> token"),
+            }
+        } else if self.model.is_gemma() {
+            match self.tokenizer.get_token("<eos>") {
+                Some(token) => token,
+                None => anyhow::bail!("cannot find the <eos> token"),
+            }
+        } else {
+            anyhow::bail!("cannot find the eos token");
         };
+         
         let start_gen = std::time::Instant::now();
         for index in 0..sample_len {
             let context_size = if index > 0 { 1 } else { tokens.len() };
@@ -205,7 +294,7 @@ struct Args {
     seed: u64,
 
     /// The length of the sample to generate (in tokens).
-    #[arg(long, short = 'n', default_value_t = 100)]
+    #[arg(long, short = 'n', default_value_t = 10000)]
     sample_len: usize,
 
     #[arg(long)]
@@ -232,8 +321,8 @@ struct Args {
     repeat_last_n: usize,
 
     /// The model to use.
-    #[arg(long, default_value = "2b")]
-    which: Which,
+    #[arg(long, default_value = "Qwen0.5b")]
+    model: Which,
 
     #[arg(long)]
     use_flash_attn: bool,
@@ -272,21 +361,32 @@ fn main() -> Result<()> {
     let api = api_builder_token.build()?;
     let model_id = match &args.model_id {
         Some(model_id) => model_id.to_string(),
-        None => match args.which {
-            Which::InstructV1_1_2B => "google/gemma-1.1-2b-it".to_string(),
-            Which::InstructV1_1_7B => "google/gemma-1.1-7b-it".to_string(),
-            Which::Base2B => "google/gemma-2b".to_string(),
-            Which::Base7B => "google/gemma-7b".to_string(),
-            Which::Instruct2B => "google/gemma-2b-it".to_string(),
-            Which::Instruct7B => "google/gemma-7b-it".to_string(),
-            Which::CodeBase2B => "google/codegemma-2b".to_string(),
-            Which::CodeBase7B => "google/codegemma-7b".to_string(),
-            Which::CodeInstruct2B => "google/codegemma-2b-it".to_string(),
-            Which::CodeInstruct7B => "google/codegemma-7b-it".to_string(),
-            Which::BaseV2_2B => "google/gemma-2-2b".to_string(),
-            Which::InstructV2_2B => "google/gemma-2-2b-it".to_string(),
-            Which::BaseV2_9B => "google/gemma-2-9b".to_string(),
-            Which::InstructV2_9B => "google/gemma-2-9b-it".to_string(),
+        None => match args.model {
+            Which::GemmaInstructV1_1_2B => "google/gemma-1.1-2b-it".to_string(),
+            Which::GemmaInstructV1_1_7B => "google/gemma-1.1-7b-it".to_string(),
+            Which::Gemma2B => "google/gemma-2b".to_string(),
+            Which::Gemma7B => "google/gemma-7b".to_string(),
+            Which::GemmaInstruct2B => "google/gemma-2b-it".to_string(),
+            Which::GemmaInstruct7B => "google/gemma-7b-it".to_string(),
+            Which::GemmaCode2B => "google/codegemma-2b".to_string(),
+            Which::GemmaCode7B => "google/codegemma-7b".to_string(),
+            Which::GemmaCodeInstruct2B => "google/codegemma-2b-it".to_string(),
+            Which::GemmaCodeInstruct7B => "google/codegemma-7b-it".to_string(),
+            Which::Gemma2_2B => "google/gemma-2-2b".to_string(),
+            Which::Gemma2Instruct2B => "google/gemma-2-2b-it".to_string(),
+            Which::Gemma2_9B => "google/gemma-2-9b".to_string(),
+            Which::Gemma2Instruct9B => "google/gemma-2-9b-it".to_string(),
+            Which::Qwen2_0_5b => "Qwen/Qwen2-0.5B".to_string(),
+            Which::Qwen2_1_5b => "Qwen/Qwen2-1.5B".to_string(),
+            Which::Qwen2_7b => "Qwen/Qwen2-7B".to_string(),
+            Which::Qwen2_72b => "Qwen/Qwen2-72B".to_string(),
+            Which::Qwen0_5b => "Qwen/Qwen1.5-0.5B".to_string(),
+            Which::Qwen1_8b => "Qwen/Qwen1.5-1.8B".to_string(),
+            Which::Qwen4b => "Qwen/Qwen1.5-4B".to_string(),
+            Which::Qwen7b => "Qwen/Qwen1.5-7B".to_string(),
+            Which::Qwen14b => "Qwen/Qwen1.5-14B".to_string(),
+            Which::Qwen72b => "Qwen/Qwen1.5-72B".to_string(),
+            Which::QwenMoeA27b => "Qwen/Qwen1.5-MoE-A2.7B".to_string(),
         },
     };
     let repo = api.repo(Repo::with_revision(
@@ -307,27 +407,43 @@ fn main() -> Result<()> {
             .split(',')
             .map(std::path::PathBuf::from)
             .collect::<Vec<_>>(),
-        None => candle_examples::hub_load_safetensors(&repo, "model.safetensors.index.json")?,
+        None => match args.model {
+            Which::Qwen0_5b | Which::Qwen2_0_5b | Which::Qwen2_1_5b | Which::Qwen1_8b => {
+                vec![repo.get("model.safetensors")?]
+            }
+            _ => candle_examples::hub_load_safetensors(&repo, "model.safetensors.index.json")?,
+        },
     };
     println!("retrieved the files in {:?}", start.elapsed());
     let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
 
     let start = std::time::Instant::now();
     let device = candle_core::Device::new_cuda(0)?;
+    // let device = candle_core::Device::new_metal(0)?;     // On Mac
     let dtype = if device.is_cuda() {
         DType::F16  // BF16
     } else {
         DType::F32
     };
     let vb = unsafe { VarBuilder::from_mmaped_safetensors(&filenames, dtype, &device)? };
-    let model = if args.which.is_v1() {
-        let config: Config1 = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
-        let model = Model1::new(args.use_flash_attn, &config, vb)?;
-        Model::V1(model)
+    let model = if args.model.is_gemma() {
+        let config: ConfigGemma = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
+        let model = ModelGemma::new(args.use_flash_attn, &config, vb)?;
+        Model::Gemma(model)
+    } else if args.model.is_gemma2() {
+        let config: ConfigGemma2 = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
+        let model = ModelGemma2::new(args.use_flash_attn, &config, vb)?;
+        Model::Gemma2(model)
+    } else if args.model.is_qwen() {
+        let config: ConfigQwen = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
+        let model = ModelQwen::new(&config, vb)?;
+        Model::Qwen(model)
+    } else if args.model.is_qwen_moe() {
+        let config: ConfigQwenMoe = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
+        let model = ModelQwenMoe::new(&config, vb)?;
+        Model::QwenMoe(model)
     } else {
-        let config: Config2 = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
-        let model = Model2::new(args.use_flash_attn, &config, vb)?;
-        Model::V2(model)
+        unreachable!()
     };
 
     println!("loaded the model in {:?}", start.elapsed());
